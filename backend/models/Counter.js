32d80274118c707e7b02b26d3649e192dae27f97
Chandrasekhar Uppu <chandrasekharuppu004@gmail.com>
2026-02-21 16:41:27 +0530

    Debug: added full error logging in register route

--- backend/models/Counter.js
+++ backend/models/Counter.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const counterSchema = new mongoose.Schema({
-    _id: { type: String, required: true },
-    seq: { type: Number, default: 1135480 }
+    _id: { type: String, required: true, unique: true },
+    seq: { type: Number, default: 0 }
 });
 
 counterSchema.statics.getNextUserId = async function () {
